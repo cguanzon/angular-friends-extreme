3cdc9670f9f21f8bb02d8fc26e859e02c353f4de
cguanzon <cguanzon@instructure.com>
2015-02-08 14:16:43 -0700

    completes up to step 6, also improves step 5

--- FriendController.js
+++ FriendController.js
@@ -2,15 +2,24 @@ var app = angular.module('AngularFriends');
 
 app.controller('FriendCtrl', function($scope, $http){
 
-    $scope.searchTermName = "";
-    $scope.searchTermLocation = "";
-
-
+    $scope.searchTermName = {};
+    $scope.sortBy = "name";
+    $scope.desc = "false"
 
     $http.get('friend-data.json')
         .then(function(res){
-            $scope.friends = res.data;
+            $scope.friends = integerFy(res.data);
         });
+
+    //converts friend_count into an integer for accurate sorting
+    var integerFy = function(friendData){
+        var intergerFied = friendData.map(function(current, index, array){
+            var fixed = current;
+            fixed.friend_count = parseInt(current.friend_count);
+            return fixed;
+        });
+        return intergerFied;
+    };
 });
 
 
