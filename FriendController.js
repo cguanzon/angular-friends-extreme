df35a06bcc8ad2ff1339c27ce6104971dda1bbb2
cguanzon <cguanzon@instructure.com>
2015-02-08 15:54:11 -0700

    completes up to Black Diamond

--- FriendController.js
+++ FriendController.js
@@ -6,19 +6,21 @@ app.controller('FriendCtrl', function($scope, $http){
     $scope.sortBy = "name";
     $scope.desc = "false"
 
+    $scope.filterOpts = ["name", "friend_count", "current_location.city",
+        "current_location.state", "current_location.country"];
+
     $http.get('friend-data.json')
         .then(function(res){
-            $scope.friends = integerFy(res.data);
+            $scope.friends = fixData(res.data);
         });
 
     //converts friend_count into an integer for accurate sorting
-    var integerFy = function(friendData){
-        var intergerFied = friendData.map(function(current, index, array){
+    var fixData = function(friendData){
+        return friendData.map(function(current, index, array){
             var fixed = current;
-            fixed.friend_count = parseInt(current.friend_count);
+            fixed.friend_count = parseInt(current.friend_count) || 0;
             return fixed;
         });
-        return intergerFied;
     };
 });
 
